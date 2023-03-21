975b326a3c38c22f877ee12e756dd5ac49e5397b
victor francois <victor.francois@uphf.fr>
2023-03-21 10:18:53 +0100

    enregistrement des modifications du fichier system_hospitalier.java

---
+++ dossier_pt2/system_hospitalier.java
@@ -0,0 +1,48 @@
+package tp_git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class System_hospitalier {
+	
+	private List<Requête> listRequetes;
+
+	public System_hospitalier(List<Requête> listRequetes) {
+		super();
+		this.listRequetes = listRequetes;
+	}
+
+	public System_hospitalier() {
+		super();
+		listRequetes = new ArrayList<>();
+	}
+
+	public List<Requête> getListRequetes() {
+		return listRequetes;
+	}
+
+	public void setListRequetes(List<Requête> listRequetes) {
+		this.listRequetes = listRequetes;
+	}
+	
+	public void recevoirRequete(Requête requete) {
+		listRequetes.add(requete);
+		String analyse =  analyserRequete(requete);
+		String comparaisonDonnees = comparerDonnes();
+		System.out.println(analyse);
+		System.out.println(comparaisonDonnees);
+	}
+	
+	private String analyserRequete(Requête requête) {
+		// Analyser la requête
+		//la methode renvoie une chaine de charactere  pour signaler que la requete a ete analyser             
+		return "requête analysée";
+	}
+	
+	private String comparerDonnes() {
+		// Comparaison des données
+		// la methode signale que le cluster a ete trouve en renvoyant une chaine de charactere 
+		return "Cluster trouvé";
+	}
+
+}
